28da5b3a44e5bd3c13130d41eeaff31a375b60fb
AhmedAbdrabou22 <ahmedabdrabou723@gmail.com>
2023-04-23 11:52:22 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -11,18 +11,17 @@ closet.addEventListener('click', () => {
 
 function openCity(evt, cityName) {
     console.log(evt);
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
+    var i
+    var tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
-        console.log(tabcontent.length);
     }
-    tablinks = document.getElementsByClassName("tablinks");
+    var tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace("active", "");
     }
     document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+    evt.currentTarget.className += "active";
 }
 
 var tablinksAll = document.querySelectorAll('.taplinks');
